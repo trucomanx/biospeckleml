01e24aea674ba96ef71a4952ea61d98b68475035
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2021-05-26 23:41:30 -0300

    Agregando extra_octave_plot.cpp

--- bintools/training.cpp
+++ bintools/training.cpp
@@ -23,7 +23,7 @@ Para executar o programa:
 
 #include "headers/extra_preproccesing.cpp"
 #include "headers/extra_postproccesing.cpp"
-
+#include "headers/extra_octave_plot.cpp"
 
 //inputs
 std::string dirpath ="/mnt/boveda/DATASETs/BIOSPECKLE/cafe-biospeckle/sem1";
@@ -37,25 +37,6 @@ std::string filename_w="LogisticModelW.dat";
 std::string filename_mean="FeatureScalingMean.dat";
 std::string filename_std="FeatureScalingStd.dat";
 
-
-void octave_plot_data( const std::string &outputpath,
-                        const Pds::Matrix &X,
-                        const Pds::Matrix &Y,
-                        const std::string &prename)
-{
-    Pds::Octave::XLabel="Mean";
-    Pds::Octave::YLabel="Std";
-    Pds::Octave::ZLabel="AVD";
-    std::string filename_m  =outputpath+Pds::Ra::FileSep+prename+".m";
-    std::string filename_png=outputpath+Pds::Ra::FileSep+prename+".png";
-    
-    std::cout<<"\n";
-    std::cout<<"+++ Printing data using OCTAVE ...\n";
-    Pds::Ra::Tic();
-    Pds::Octave::Plot::ScatterX3DY( X,Y,filename_m,filename_png);
-    std::cout<<"+++ ";Pds::Ra::Toc();
-    std::cout<<"+++ Files "+outputpath+Pds::Ra::FileSep+prename+".* were printed.\n";
-}
 
 int main(void)
 {
